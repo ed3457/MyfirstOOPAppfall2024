30251a618c77ce7af5e4be0532910221599e7bf3
Kamel R <kamel.r@wayne.edu>
2024-09-04 18:27:58 -0400

    added code to the cpp file for the student class

--- MyfirstOOPAppfall2024.cpp
+++ MyfirstOOPAppfall2024.cpp
@@ -2,7 +2,7 @@
 //
 
 #include <iostream>
-
+#include "Student.h"
 int main()
 {
     std::cout << "Hello World!\n";
--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,40 @@
 #include "Student.h"
+#include <iostream>
+using namespace std; 
+void Student::setID(string i)
+{
+	id = i;
+
+}
+
+string Student::getID()
+{
+	return id;
+}
+
+void Student::setMajor(string m)
+{
+	major = m;
+}
+
+string Student::getMajor()
+{
+	return major;
+}
+
+void Student::setName(string n)
+{
+	name = n;
+}
+
+string Student::getName()
+{
+	return name;
+}
+
+void Student::printStudentInfo()
+{
+	cout << "Student ID:" << id << endl;
+	cout << "Student Name:" << name << endl;
+	cout << "Student Major:" << major << endl;
+}
